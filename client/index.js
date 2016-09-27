fc96bb10a660a0f3ed98606f2e30735773eb1429
hadiab <abdalhadi.m92@gmail.com>
2016-09-27 18:31:22 +0300

    Sends data to server via redux thunk action

--- client/index.js
+++ client/index.js
@@ -2,5 +2,16 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
-render(<Router history={browserHistory} routes={routes} />, document.getElementById('app'));
+const store = createStore(
+  (state = {}) => state,
+  applyMiddleware(thunk)
+);
+
+render(
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>, document.getElementById('app'));
